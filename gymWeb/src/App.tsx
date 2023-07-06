57dd367183bef87f2e032fb447f2e298d3e836d0
gastonfalena <gastonfalena@gmail.com>
2023-07-06 09:04:32 -0300

    iRHzDkOu creating proyect finish, installing react-router-dom

--- gymWeb/src/App.tsx
+++ gymWeb/src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import Contacto from './components/Contacto';
 function App() {
-    return <>GYMWEB coming soon...</>;
+    return (
+        <>
+            <NavBar />
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/Login' element={<Login />} />
+                    <Route path='/Contacto' element={<Contacto />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
