6b37a488a1c78dd5354f36a5e1aa366f71512b5a
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-07-06 11:30:28 -0300

    wpOsptfK developing login forms

--- gymWeb/src/components/Login/Login.tsx
+++ gymWeb/src/components/Login/Login.tsx
@@ -1,3 +1,41 @@
-export default function Login() {
-    return <div>Login</div>;
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            aria-label="Username"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            aria-label="Password"
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Si no estás registrado y quieres hacerlo haz,
+        <Link to="/Register"> click aquí</Link>.
+      </p>
+    </div>
+  );
+};
+
+export default Login;
