636b5dcc3e6eb8a1834fca14bc29efdfba1ff158
hachecom <antocomisso@gmail.com>
2023-07-19 19:54:19 -0300

    UN8C5L4h added new columns to the admin table, css for the delete and edit buttons, search filter for the table.

--- gymWeb/src/components/Admin/Admin.tsx
+++ gymWeb/src/components/Admin/Admin.tsx
@@ -1,11 +1,17 @@
 import TableAdmin from '../TableAdmin/TableAdmin';
 import styles from '../Admin/Admin.module.css';
+
+let loggeduser = 'Administrador';
+//tomar el nombre de la sesión o la db para la página de usuario logueado.
+
 const users = [
     {
         id: 0,
         name: 'juan',
         lastname: 'perez',
         dni: 12345678,
+        dateofbirth: '12/01/1990',
+        phonenumber: 12121212,
         email: 'antocomisso@gmail.com',
         status: 'active',
     },
@@ -14,6 +20,8 @@ const users = [
         name: 'ana',
         lastname: 'gómez',
         dni: 12345678,
+        dateofbirth: '12/01/1990',
+        phonenumber: 12121212,
         email: 'antocomisso@gmail.com',
         status: 'active',
     },
@@ -22,6 +30,8 @@ const users = [
         name: 'fffff',
         lastname: 'ccccc',
         dni: 1111111,
+        dateofbirth: '12/01/1990',
+        phonenumber: 12121212,
         email: 'antocomisso@gmail.com',
         status: 'active',
     },
@@ -30,6 +40,8 @@ const users = [
         name: 'hhhhhh',
         lastname: 'kkkkkkk',
         dni: 2222222,
+        dateofbirth: '12/01/1990',
+        phonenumber: 12121212,
         email: 'antocomisso@gmail.com',
         status: 'active',
     },
@@ -38,6 +50,8 @@ const users = [
         name: 'sssssss',
         lastname: 'ddddddd',
         dni: 333333,
+        dateofbirth: '12/01/1990',
+        phonenumber: 12121212,
         email: 'antocomisso@gmail.com',
         status: 'active',
     },
@@ -46,8 +60,8 @@ const users = [
 const Admin: React.FC = () => {
     return (
         <div className={styles.admin}>
-            Bienvenido: Administrador
-            <TableAdmin users={users} />;
+            Bienvenido: {loggeduser}
+            <TableAdmin users={users} />
         </div>
     );
 };
--- gymWeb/src/components/TableAdmin/TableAdmin.tsx
+++ gymWeb/src/components/TableAdmin/TableAdmin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TableAdmin.module.css';
 
 interface User {
@@ -6,6 +6,8 @@ interface User {
     name: string;
     lastname: string;
     dni: number;
+    dateofbirth: string;
+    phonenumber: number;
     email: string;
     status: string;
 }
@@ -15,35 +17,63 @@ interface TableProps {
 }
 
 const TableAdmin: React.FC<TableProps> = ({ users }) => {
+    const [filterTerm, setFilterTerm] = useState('');
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterTerm(event.target.value.toUpperCase());
+    };
+
+    const filteredUsers = filterTerm
+        ? users.filter(user => user.name.toUpperCase().startsWith(filterTerm))
+        : users;
     return (
-        <table className={styles.usertable}>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Apellido</th>
-                    <th>DNI</th>
-                    <th>Email</th>
-                    <th>Estado de Cuota</th>
-                    {/* Add more table headers as per your requirements */}
-                </tr>
-            </thead>
-            <tbody>
-                {users.map(user => (
-                    <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.lastname}</td>
-                        <td>{user.dni}</td>
-                        <td>{user.email}</td>
-                        <td>{user.status}</td>
-                        {/* Add more table cells based on the user object properties */}
-                        <td>
-                            <button className={styles.button}>eliminar</button>{' '}
-                            <button className={styles.button}>editar</button>
-                        </td>
+        <>
+            <div className={styles.filter}>
+                Filtrar por nombre:{' '}
+                <input
+                    className={styles.filterinput}
+                    type='text'
+                    placeholder='Filtrar por nombre'
+                    value={filterTerm}
+                    onChange={handleFilterChange}
+                />
+            </div>
+
+            <table className={styles.usertable}>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>DNI</th>
+                        <th>Fecha de Nac.</th>
+                        <th>Teléfono</th>
+                        <th>Email</th>
+                        <th>Estado de Cuota</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {filteredUsers.map(user => (
+                        <tr key={user.id}>
+                            <td>
+                                {user.name} {user.lastname}
+                            </td>
+                            <td>{user.dni}</td>
+                            <td>{user.dateofbirth}</td>
+                            <td>{user.phonenumber}</td>
+                            <td>{user.email}</td>
+                            <td>{user.status}</td>
+                            <td>
+                                <button className={styles.buttondelete}>
+                                    Eliminar
+                                </button>{' '}
+                                <button className={styles.buttonedit}>
+                                    Editar
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 };
 
