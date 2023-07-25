6ff3f5753c1988b79b7d95ba20dcd65013ffe8d3
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-07-25 16:22:28 -0300

    wpOsptfK adding the reset to the login form

--- gymWeb/src/components/Login/Login.tsx
+++ gymWeb/src/components/Login/Login.tsx
@@ -1,41 +1,53 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import styles from './Login.module.css';
 
 const Login: React.FC = () => {
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-  };
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Usuario:</label>
-          <input
-            type="text"
-            id="username"
-            aria-label="Username"
-            required
-          />
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        // lógica inicio de sesión 
+        setUsername('');
+        setPassword('');
+    };
+
+    return (
+        <div className={styles.loginContainer}>
+            <h2>Login</h2>
+            <form className={styles.loginForm} onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='username'>Usuario:</label>
+                    <input
+                        type='text'
+                        id='username'
+                        aria-label='Username'
+                        required
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Contraseña:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        aria-label='Password'
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type='submit'>Login</button>
+            </form>
+            <p>
+                Si no estás registrado y quieres hacerlo, haz
+                <Link to='/register'> click aquí</Link>.
+            </p>
         </div>
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            aria-label="Password"
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        Si no estás registrado y quieres hacerlo haz,
-        <Link to="/Register"> click aquí</Link>.
-      </p>
-    </div>
-  );
+    );
 };
 
 export default Login;
