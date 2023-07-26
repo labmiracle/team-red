8246bd4f57efacdc6c0452a993f3c55af5340012
tamaragrun <130572131+tamaragrun@users.noreply.github.com>
2023-07-26 12:16:13 -0300

    5gZM9S8G  adding contact form page

--- gymWeb/src/components/Contacto/Contacto.tsx
+++ gymWeb/src/components/Contacto/Contacto.tsx
@@ -1,3 +1,66 @@
-export default function Contacto() {
-    return <div>Contacto</div>;
-}
+import { useState } from 'react';
+import styles from './contacto.module.css';
+
+const Contacto = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //lógica para enviar el formulario
+        console.log(formData);
+    };
+
+    return (
+        <div className={styles.contactForm}>
+            <h2>Contacto</h2>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.formGroup}>
+                    <label htmlFor="name">Nombre</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="email">Correo electrónico</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="message">Mensaje</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contacto;
