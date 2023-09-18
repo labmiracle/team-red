86b6f6d06bcb0daadf3282a1dae06313e5a993ac
gastonfalena <gastonfalena@gmail.com>
2023-09-18 17:12:57 -0300

    calculating if the user is active, formatting dates

--- gymWeb/src/interfaces/User.interface.tsx
+++ gymWeb/src/interfaces/User.interface.tsx
@@ -1,4 +1,20 @@
 export interface IUser {
+    id: number;
+    firstname: string;
+    lastname: string;
+    dni: number;
+    dateofbirth: string;
+    phone: number;
+    email: string;
+    address: string;
+    city: string;
+    state: string;
+    username: string;
+    password: string;
+    pay_date: string;
+    role_id?: number | undefined;
+}
+export interface IUserRegister {
     id?: number | undefined;
     firstname: string;
     lastname: string;
